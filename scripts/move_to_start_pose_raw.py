f629198a27af856a3b2bc39fb9b3fd19bfd5a38a
Carlo Rizzardo <cr.git.mail@gmail.com>
2021-01-29 14:30:42 +0100

    Created panda_generic.launch and updated configs and xacros accordingly. Can correctly launch panda arm with: effort or position control, gripper or no gripper, generic robot name.

--- scripts/move_to_start_pose_raw.py
+++ scripts/move_to_start_pose_raw.py
@@ -2,6 +2,7 @@
 
 import rospy
 import actionlib
+import argparse
 from control_msgs.msg import FollowJointTrajectoryAction
 from control_msgs.msg import FollowJointTrajectoryResult
 from control_msgs.msg import FollowJointTrajectoryGoal
@@ -37,34 +38,44 @@ def waitForControllersStart(controllerNames, listControllers_service):
         if not allStarted:
             rospy.sleep(1)
 
+if __name__ == "__main__":
+    ap = argparse.ArgumentParser()
+    ap.add_argument("--traj_controller_name", required=False, default="panda_arm_effort_trajectory_controller", type=str, help="Topic namespace name for the trajectory contorller to use")
+    ap.add_argument("--robot_name", required=False, default="panda", type=str, help="Topic namespace name for the trajectory contorller to use")
+    args = vars(ap.parse_known_args()[0])
+    rospy.init_node('move_to_start_pose', anonymous=True, log_level=rospy.INFO)
 
-rospy.init_node('move_to_start_pose', anonymous=True, log_level=rospy.INFO)
+    controllerNamespace = args["traj_controller_name"]
 
-controllerActionName = '/panda_arm_effort_trajectory_controller/follow_joint_trajectory'
-controllerClient = actionlib.SimpleActionClient(controllerActionName, FollowJointTrajectoryAction)
-rospy.logwarn("Waiting for "+str(controllerActionName)+"...")
-controllerClient.wait_for_server(rospy.Duration(30))
+    controllerActionName = "/"+controllerNamespace+"/follow_joint_trajectory"
+    controllerClient = actionlib.SimpleActionClient(controllerActionName, FollowJointTrajectoryAction)
+    rospy.logwarn("Waiting for "+str(controllerActionName)+"...")
+    controllerClient.wait_for_server(rospy.Duration(30))
 
-listControllers_service = buildServiceProxy("/controller_manager/list_controllers", ListControllers)
-waitForControllersStart(["panda_arm_effort_trajectory_controller"], listControllers_service)
+    listControllers_service = buildServiceProxy("/controller_manager/list_controllers", ListControllers)
+    waitForControllersStart([controllerNamespace], listControllers_service)
 
 
-goal = FollowJointTrajectoryGoal()
-goal.trajectory.joint_names = ['panda_joint1','panda_joint2','panda_joint3','panda_joint4','panda_joint5','panda_joint6','panda_joint7']
-point = JointTrajectoryPoint()
-point.positions = [0, 0, 0, -1, 0, 1, 0]
-point.effort = [0, 0, 0, 0, 0, 0, 0]
-point.time_from_start = rospy.Duration(10)
-goal.trajectory.points = [point]
+    goal = FollowJointTrajectoryGoal()
+    goal.trajectory.joint_names = [ args["robot_name"]+'_joint1',
+                                    args["robot_name"]+'_joint2',
+                                    args["robot_name"]+'_joint3',
+                                    args["robot_name"]+'_joint4',
+                                    args["robot_name"]+'_joint5',
+                                    args["robot_name"]+'_joint6',
+                                    args["robot_name"]+'_joint7']
+    point = JointTrajectoryPoint()
+    point.positions = [0, 0, 0, -1, 0, 1, 0]
+    point.effort = [0, 0, 0, 0, 0, 0, 0]
+    point.time_from_start = rospy.Duration(10)
+    goal.trajectory.points = [point]
 
-controllerClient.send_goal(goal)
-rospy.logwarn("Waiting for action completion...")
-controllerClient.wait_for_result(point.time_from_start+rospy.Duration(5))
-r = controllerClient.get_result()
-rospy.logwarn("Action finished or timed out")
-
-if r.error_code == FollowJointTrajectoryResult.SUCCESSFUL:
-    rospy.loginfo("Moved successfully to start pose")
-else:
-    rospy.logerr("Failed to move to start pose. Result is: "+str(r))
-    exit(1)
+    controllerClient.send_goal(goal)
+    rospy.logwarn("Waiting for action completion...")
+    r = controllerClient.wait_for_result(point.time_from_start+rospy.Duration(5))
+    if r:
+        if controllerClient.get_result().error_code == FollowJointTrajectoryResult.SUCCESSFUL:
+            rospy.loginfo("Moved successfully to start pose")
+    else:
+        rospy.logerr("Failed to move to start pose. Result is: "+str(controllerClient.get_result()))
+        exit(1)
